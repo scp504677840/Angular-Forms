3bb128a4dcf3b4568a52acbfa35b99c1be76534f
y1笑而过 <15335747148@163.com>
2018-07-21 23:35:31 +0800

    模拟服务器表单数据
    模拟服务器表单数据

---
+++ 响应式表单/模拟服务器表单数据/flab/src/app/address.ts
@@ -0,0 +1,6 @@
+export class Address {
+  street = '';
+  city = '';
+  state = '';
+  zip = '';
+}
---
+++ 响应式表单/模拟服务器表单数据/flab/src/app/hero.ts
@@ -0,0 +1,7 @@
+import {Address} from './address';
+
+export class Hero {
+  id = 0;
+  name = '';
+  addresses: Address[];
+}
